16850bdfb75941efbf97b42b4239bd435e1bb466
Armando Couto <coutoarmando@gmail.com>
2023-10-31 11:44:07 -0300

    Ajuste

--- lib/flutter_carplay.dart
+++ lib/flutter_carplay.dart
@@ -1,40 +1,40 @@
-export "package:flutter_carplay/helpers/enum_utils.dart";
+export "package:flutter_carplaybr/helpers/enum_utils.dart";
 export 'package:flutter_carplaybr/constants/constants.dart'
     show CPConnectionStatusTypes;
-export "package:flutter_carplay/models/action_sheet/action_sheet_template.dart"
+export "package:flutter_carplaybr/models/action_sheet/action_sheet_template.dart"
     show CPActionSheetTemplate;
-export "package:flutter_carplay/models/alert/alert_action.dart"
+export "package:flutter_carplaybr/models/alert/alert_action.dart"
     show CPAlertAction;
-export "package:flutter_carplay/models/alert/alert_template.dart"
+export "package:flutter_carplaybr/models/alert/alert_template.dart"
     show CPAlertTemplate;
-export "package:flutter_carplay/models/alert/alert_constants.dart"
+export "package:flutter_carplaybr/models/alert/alert_constants.dart"
     show CPAlertActionStyles;
-export "package:flutter_carplay/models/button/bar_button.dart" show CPBarButton;
-export "package:flutter_carplay/models/button/alert_constants.dart"
+export "package:flutter_carplaybr/models/button/bar_button.dart" show CPBarButton;
+export "package:flutter_carplaybr/models/button/alert_constants.dart"
     show CPBarButtonStyles;
-export "package:flutter_carplay/models/grid/grid_button.dart" show CPGridButton;
-export "package:flutter_carplay/models/grid/grid_template.dart"
+export "package:flutter_carplaybr/models/grid/grid_button.dart" show CPGridButton;
+export "package:flutter_carplaybr/models/grid/grid_template.dart"
     show CPGridTemplate;
-export "package:flutter_carplay/models/information/information_item.dart"
+export "package:flutter_carplaybr/models/information/information_item.dart"
     show CPInformationItem;
-export "package:flutter_carplay/models/information/information_template.dart"
+export "package:flutter_carplaybr/models/information/information_template.dart"
     show CPInformationTemplate;
 export 'package:flutter_carplaybr/models/information/information_constants.dart'
     show CPInformationTemplateLayout;
-export "package:flutter_carplay/models/list/list_constants.dart"
+export "package:flutter_carplaybr/models/list/list_constants.dart"
     show CPListItemAccessoryTypes, CPListItemPlayingIndicatorLocations;
-export "package:flutter_carplay/models/list/list_item.dart" show CPListItem;
-export "package:flutter_carplay/models/list/list_section.dart"
+export "package:flutter_carplaybr/models/list/list_item.dart" show CPListItem;
+export "package:flutter_carplaybr/models/list/list_section.dart"
     show CPListSection;
-export "package:flutter_carplay/models/list/list_template.dart"
+export "package:flutter_carplaybr/models/list/list_template.dart"
     show CPListTemplate;
-export "package:flutter_carplay/models/poi/poi.dart"
+export "package:flutter_carplaybr/models/poi/poi.dart"
     show CPPointOfInterest;
-export "package:flutter_carplay/models/poi/poi_template.dart"
+export "package:flutter_carplaybr/models/poi/poi_template.dart"
     show CPPointOfInterestTemplate;
-export "package:flutter_carplay/models/tabbar/tabbar_template.dart"
+export "package:flutter_carplaybr/models/tabbar/tabbar_template.dart"
     show CPTabBarTemplate;
-export "package:flutter_carplay/models/button/text_button.dart"
+export "package:flutter_carplaybr/models/button/text_button.dart"
     show CPTextButton;
 
 export 'package:flutter_carplaybr/carplay_worker.dart' show FlutterCarplay;
