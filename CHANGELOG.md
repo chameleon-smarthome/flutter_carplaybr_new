38ca9138087e07be813c24ad4d60e430cad473c7
Armando Couto <coutoarmando@gmail.com>
2023-10-31 12:17:43 -0300

    Nova lib

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 1.1.10
+## 1.1.11
 
 - Build fix for the issue [#7](https://github.com/oguzhnatly/flutter_carplay/issues/7)
 - 
--- ios/Classes/FlutterCarplayPlugin.h
+++ ios/Classes/FlutterCarplayPlugin.h
@@ -1,4 +1,4 @@
 #import <Flutter/Flutter.h>
 
-@interface FlutterCarplayPlugin : NSObject<FlutterPlugin>
+@interface FlutterCarplaybrPlugin : NSObject<FlutterPlugin>
 @end
